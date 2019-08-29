abfeef9a1634eb526575ea0eb1f43158ce3342d4
Trevor Eames <mrtvor@gmail.com>
2019-08-28 22:13:03 -0400

    :tada: initial commit

---
+++ Dispositions/Mindless.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NPCTools.Desktop.Dispositions {
+
+
+    class MindlessDisposition
+    {
+        /**
+        * Mindless Disposition describes the dispositions of creatures
+        * with extremely low levels of intellect. This includes slimes,
+        * molds, jellyfish, etc.
+        */
+        public enum Mindless {
+            NORMAL,
+            DOCILE,
+            CAUTIOUS,
+            AGGRESSIVE
+        }
+
+        public static Mindless GetRandomPersonality()
+        {
+            Random random = new Random();
+            int x = random.Next(1, 98);
+
+            if (x <= 80) return Mindless.NORMAL;
+            else if (x <= 90) return Mindless.AGGRESSIVE;
+            else if (x <= 95) return Mindless.CAUTIOUS;
+
+            return Mindless.DOCILE;
+        }
+    }
+
+}
---
+++ Mobs/Slime.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NPCTools.Desktop.Mobs {
+
+    public class Slime {
+
+        private Dispositions.MindlessDisposition.Mindless disposition;
+
+        public Slime() {
+            disposition = Dispositions.MindlessDisposition.GetRandomPersonality();
+        }
+    }
+}
