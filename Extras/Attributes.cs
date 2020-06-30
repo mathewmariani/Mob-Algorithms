aafeb3ce175c28630d800ec6ad48fe8f94734d4c
Mat Mariani <mat.mariani@hotmail.com>
2020-06-29 21:37:15 -0400

    added properties to Attributes

--- Extras/Attributes.cs
+++ Extras/Attributes.cs
@@ -2,69 +2,38 @@
 namespace MOBAlgorithms.Desktop.Extras
 {
     public class Attributes
-    {
-        private int totalHealth = 100;
-        private int totalMana = 0;
-        private int health = 0;
-        private int mana = 0;
-
-        private int strength;
-        private int dexterity;
-        private int constitution;
-        private int intelligence;
-        private int wisdom;
-        private int charisma;
-
+    {      
         public Attributes(int health, int mana, int strength, int dexterity, int constitution, int intelligence, int wisdom, int charisma)
         {
-            this.health = health;
-            this.mana = mana;
-            this.strength = strength;
-            this.dexterity = dexterity;
-            this.constitution = constitution;
-            this.intelligence = intelligence;
-            this.wisdom = wisdom;
-            this.charisma = charisma;
+            this.Health = health;
+            this.Mana = mana;
+            this.Strength = strength;
+            this.Dexterity = dexterity;
+            this.Constitution = constitution;
+            this.Intelligence = intelligence;
+            this.Wisdom = wisdom;
+            this.Charisma = charisma;
         }
 
-        public Attributes()
-        {}
+        public int Health { get; set; } = 0;
+        public int Mana { get; set; } = 0;
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Constitution { get; set; }
+        public int Intelligence { get; set; }
+        public int Wisdom { get; set; }
+        public int Charisma { get; set; }
 
-        //Health
-        public int getTotalHealth() { return totalHealth; }
-        public void setTotalHealth(int totalHealth) { this.totalHealth = totalHealth; }
-        public int getHealth() { return health; }
-        public void setHealth(int health) { this.health = health; }
-        public void recoverHealth(int points) { this.health = (this.health + points > totalHealth) ? totalHealth : this.health + points; }
-        public void damage(int points) { this.health = (this.health - points > 0) ? this.health - points : 0; }
+        public int TotalHealth { get; set; } = 100;
+        public int TotalMana { get; set; } = 0;
 
-        //Mana
-        public int getTotalMana() { return totalMana; }
-        public void setTotalMana(int totalMana) { this.totalMana = totalMana; }
-        public int getMana() { return mana; }
-        public void setMana(int mana) { this.mana = mana; }
-        public void recoverMana(int points) { this.mana = (this.mana + points > totalMana) ? totalMana : this.mana + points; }
-        public void expendMana(int points) { this.mana = (this.mana - points > 0) ? this.mana - points : 0; }
-        public void manaDrain() { this.mana = 0; }
+        // Health helper functions
+        public void recoverHealth(int points) { this.Health = (this.Health + points > this.TotalHealth) ? this.TotalHealth : this.Health + points; }
+        public void damage(int points) { this.Health = (this.Health - points > 0) ? this.Health - points : 0; }
 
-
-        //Skills
-        public int getStr() { return strength; }
-        public void setStr(int strength) { this.strength = strength; }
-
-        public int getDex() { return dexterity; }
-        public void setDex(int dexterity) { this.dexterity = dexterity; }
-
-        public int getCon() { return constitution; }
-        public void setCon(int constitution) { this.constitution = constitution; }
-
-        public int getInt() { return intelligence; }
-        public void setInt(int intelligence) { this.intelligence = intelligence; }
-
-        public int getWis() { return wisdom; }
-        public void setWis(int wisdom) { this.wisdom = wisdom; }
-
-        public int getChar() { return charisma; }
-        public void setChar(int charisma) { this.charisma = charisma; }
+        // Mana helper functions
+        public void recoverMana(int points) { this.Mana = (this.Mana + points > this.TotalMana) ? this.TotalMana : this.Mana + points; }
+        public void expendMana(int points) { this.Mana = (this.Mana - points > 0) ? this.Mana - points : 0; }
+        public void manaDrain() { this.Mana = 0; }
     }
 }
